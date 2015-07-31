11b29beb789ce4de33b5dc56cd0fb29cd48025d2
Mike.Salyh <salyhm@gmail.com>
2015-07-30 22:12:29 -0700

    Vox: The simplest text-to-speech has been implemented.

---
+++ modules/vox/Vox_0.1/Vox_0.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace SampleSynthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Initialize a new instance of the SpeechSynthesizer.
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            Console.WriteLine("Enter something for me to say:");
+            Console.WriteLine();
+            String myLine = Console.ReadLine();
+
+            // Configure the audio output. 
+            synth.SetOutputToDefaultAudioDevice();
+            synth.SelectVoice("Microsoft Zira Desktop");
+
+            // Speak a string asynchronously.
+            synth.SpeakAsync(myLine);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
