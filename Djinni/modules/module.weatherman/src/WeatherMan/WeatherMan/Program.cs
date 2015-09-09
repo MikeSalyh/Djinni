ea0e231aa9aafcfe2e11b737c2ee9d78050a3ef8
Mike.Salyh <salyhm@gmail.com>
2015-09-08 23:13:31 -0700

    WeatherMan: Modularized Weatherman. (v0.5)
    Weatherman can now also get the weather from anywhere, passed a ZIP code.

---
+++ Djinni/modules/module.weatherman/src/WeatherMan/WeatherMan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.WeatherMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherMan wm = new WeatherMan("b3b04a47e7db69a0");
+            WeatherReport w = wm.GetWeatherReport();
+            Console.ReadLine();
+        }
+    }
+}
