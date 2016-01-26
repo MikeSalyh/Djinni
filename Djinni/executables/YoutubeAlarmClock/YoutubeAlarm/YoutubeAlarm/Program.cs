67e8c99163b2cc05b9d03b3e8a0c64b23cd6b260
Mike.Salyh <salyhm@gmail.com>
2016-01-25 20:32:58 -0800

    YoutubeAlarm: Starting an ad-hoc program that plays YouTube songs in the morning.

---
+++ Djinni/executables/YoutubeAlarmClock/YoutubeAlarm/YoutubeAlarm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace YoutubeAlarm
+{
+    /// <summary>
+    /// This program is a standalone executable.
+    /// When launched, it opens a YouTube video from a series of choices at random.
+    /// This program doesn't integrate into the larger Djinni framework. It's purely ad hoc. 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world.");
+            System.Console.WriteLine("Press any key to terminate...");
+            System.Console.ReadKey();
+        }
+    }
+}
